2ea5116ec249e2515b42c5d44648874d0917d728
Tom Bee <tburzynski2@yahoo.com>
2024-05-02 16:21:46 -0500

    Created inquirer questions array

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,82 @@ const fs = require("fs");
 import inquirer from "inquirer";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter your project's title:",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Enter your project's description:",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Enter installation steps:",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Enter steps for usage:",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "How can other developers contribute to your project?",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "How can other developers run tests on your project's code?",
+  },
+  {
+    type: "checkbox",
+    message: "Choose your project's license:",
+    name: "stack",
+    choices: [
+      "Creative Commons license family",
+      "Creative Commons Zero v1.0 Universal",
+      "Creative Commons Attribution 4.0",
+      "Creative Commons Attribution ShareAlike 4.0",
+      "Do What The F*ck You Want To Public License",
+      "Educational Community License v2.0",
+      "Eclipse Public License 1.0",
+      "Eclipse Public License 2.0",
+      "European Union Public License 1.1",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License family",
+      "GNU General Public License v2.0",
+      "GNU General Public License v3.0",
+      "GNU Lesser General Public License family",
+      "GNU Lesser General Public License v2.1",
+      "GNU Lesser General Public License v3.0",
+      "ISC",
+      "LaTeX Project Public License v1.3c",
+      "Microsoft Public License",
+      "MIT",
+      "Mozilla Public License 2.0",
+      "Open Software License 3.0",
+      "PostgreSQL License",
+      "SIL Open Font License 1.1",
+      "University of Illinois/NCSA Open Source License",
+      "The Unlicense",
+      "zLib License",
+    ],
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Enter your Github username:",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Enter your email address:",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
